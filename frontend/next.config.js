2b28aed20e6fa54a0c47f1c885bca8ac60f95188
NextFire <git@yuru.moe>
2022-03-31 11:25:40 +0000

    [front] api proxy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: "/api/:path*",
+          destination: "http://127.0.0.1:8080/projetappliweb/api/:path*",
+        },
+      ],
+    };
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
