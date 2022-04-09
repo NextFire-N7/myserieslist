9ad6ea43d7eb1c0d9b9b88003d4a1e01d65a1e6d
NextFire <git@yuru.moe>
2022-04-09 19:09:25 +0200

    [front] api fetch example

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,31 +1,97 @@
 import type { NextPage, GetServerSideProps } from "next";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 // server-side
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const resp = await fetch("http://localhost:3000/api/helloworld");
+  const resp = await fetch("http://localhost:3000/api/persons");
   const data = await resp.json();
   return { props: data };
 };
 
-const Home: NextPage<{ message: string }> = ({ message }) => {
-  const [hello, setHello] = useState<string>();
+// Consumes the result of getServerSideProps
+const Home: NextPage<{ message: string; persons: Person[] }> = ({
+  message,
+  persons,
+}) => {
+  // store the api response
+  const [data, setData] = useState<{ message: string; persons: Person[] }>();
+  // trick to force the component to fetch data from the api again after a submit
+  const [didChange, setDidChange] = useState(false);
 
   // client-side
+  // is called each time the didChange dependancy changes
   useEffect(() => {
-    fetch("/api/helloworld")
+    fetch("/api/persons")
       .then((resp) => resp.json())
-      .then((data) => setHello(data.message));
-  }, []);
+      .then((data) => setData(data));
+  }, [didChange]);
+
+  // calls the api to add a new person, and change didChange
+  const handleSubmit = useCallback(
+    async (e) => {
+      e.preventDefault();
+      const resp = await fetch("/api/persons", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          lastName: e.target.elements.lastName.value,
+          firstName: e.target.elements.firstName.value,
+        }),
+      });
+      const data = await resp.json();
+      console.log(data);
+      setDidChange(!didChange);
+    },
+    [didChange]
+  );
 
   return (
     <div>
-      <h1 className="underline">GET /api/helloworld (server-side):</h1>
-      <p>{message}</p>
-      <h1 className="underline">GET /api/helloworld (client-side):</h1>
-      <p>{hello}</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="lastName" placeholder="Last Name" />
+        <input type="text" name="firstName" placeholder="First Name" />
+        <input type="submit" value="Add" />
+      </form>
+
+      <div className="flex space-x-10">
+        <div>
+          <h1 className="underline">GET /api/persons (server-side):</h1>
+          <p>message: {message}</p>
+          <p>persons:</p>
+          <ul>
+            {persons.map((person) => (
+              <li key={person.id}>
+                {person.lastName} {person.firstName}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          <h1 className="underline">GET /api/persons (client-side):</h1>
+          {data && (
+            <>
+              <p>message: {data.message}</p>
+              <p>persons:</p>
+              <ul>
+                {data.persons.map((person) => (
+                  <li key={person.id}>
+                    {person.lastName} {person.firstName}
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Home;
+
+interface Person {
+  id: number;
+  lastName: string;
+  firstName: string;
+}
