384a00c5b66dbbdd2815330348aaa156b09c1e95
NextFire <git@yuru.moe>
2022-04-21 09:54:01 +0000

    [back] mediaservice missing pathparam

--- backend/src/main/java/moe/yuru/myserieslist/services/MediasService.java
+++ backend/src/main/java/moe/yuru/myserieslist/services/MediasService.java
@@ -12,6 +12,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -36,7 +37,7 @@ public class MediasService {
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Media mediasGetById(int id) {
+    public Media mediasGetById(@PathParam("id") int id) {
         return em.find(Media.class, id);
     }
 
