eeb713441e712734f048ea8ecca0292a163cb88b
Dino Gurnari <dinogurnari@gmail.com>
2022-05-27 14:26:21 +0000

    oublie ID dans schedule

--- backend/src/main/java/moe/yuru/myserieslist/entities/AiringSchedule.java
+++ backend/src/main/java/moe/yuru/myserieslist/entities/AiringSchedule.java
@@ -4,10 +4,12 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class AiringSchedule implements Serializable {
     
+    @Id
     private Date scheduleDate;
 
     public Date getScheduleDate() {
